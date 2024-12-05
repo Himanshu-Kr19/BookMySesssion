419295b0491528cdeb5004ad8ffbc3230ee425d6
Ujjawal-Kantt <uk1287@srmist.edu.in>
2024-12-05 14:22:13 +0530

    Backend: Added Vercel.json file for deployement purpose.

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    next();
+});
+
 // Test the database connection
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
